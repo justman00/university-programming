9640c7ef2b7928b8d2e71900be35b0a067903c0e
Vladimir Turcan <vladimir.turcan@sumup.com>
2020-12-01 07:23:08 +0200

    finish emprirical analysis of algorithms for APA

---
+++ apa/lucrare-grafica/index.js
@@ -0,0 +1,106 @@
+const CliGraph = require('cli-graph');
+
+const constant_time_operation = () => 1 + 1;
+
+// O(log(n))
+const alg_one = (n) => {
+  let i = 0;
+  let h = 1;
+
+  while (h <= n) {
+    constant_time_operation();
+    n = n / 2;
+    i++;
+  }
+
+  return i;
+};
+
+// O(n*8^n)
+const alg_two = (n) => {
+  let iter = 0;
+  let k = 1;
+
+  for (let i = 0; i < n - 1; i++) {
+    k = 8 * k;
+    iter++;
+
+    for (let j = 1; j < k; j++) {
+      constant_time_operation();
+      iter++;
+    }
+  }
+
+  return iter;
+};
+
+// O(n*log(n))??
+const alg_three = (n) => {
+  let iter = 0;
+  let i = 2;
+
+  while (i <= n) {
+    constant_time_operation();
+    let j = 2 * i;
+    iter++;
+
+    while (j <= n) {
+      constant_time_operation();
+      j = j + i;
+      iter++;
+    }
+
+    i++;
+  }
+
+  return iter;
+};
+
+// O(n * log(n))
+const alg_four = (n) => {
+  let iter = 0;
+  let i = 2;
+
+  while (i <= n) {
+    constant_time_operation();
+    let j = 1;
+    iter++;
+
+    while (j <= n) {
+      constant_time_operation();
+      j = 2 * j;
+      iter++;
+    }
+
+    i++;
+  }
+
+  return iter;
+};
+
+function main() {
+  const dataSets = [2, 3, 4, 5];
+  const algos = [alg_one, alg_two, alg_three, alg_four];
+
+  algos.forEach((alg, i) => {
+    const graph = new CliGraph({
+      height: 20,
+      width: 8,
+      center: {
+        y: 19,
+        x: 3,
+      },
+    });
+
+    dataSets.forEach((dataSet) => {
+      const data = alg(dataSet);
+      console.log(dataSet, data, alg);
+      graph.addPoint(dataSet, data);
+    });
+
+    console.log(`This is the ${i + 1} algorithm`);
+    console.log(graph.toString());
+  });
+}
+
+main();
--- mmc-1/optimizare-neliniara.js
+++ mmc-1/optimizare-neliniara.js
@@ -1,7 +1,21 @@
-const functiaInitiala = (x1, x2) =>
-  2 * Math.pow(x1, 2) + 2 * x1 * x2 + 3 * x2 - 2 * x1 - 3 * x2;
-const grad1 = (x1, x2) => 4 * x1 + 2 * x2 - 2;
-const grad2 = (x1, x2) => 2 * x1 + 6 * x2 - 3;
+const E_CONSTANT = 2.718;
+
+const probl1 = (x1, x2) =>
+  Math.pow(x1, 2) + 2 * x1 * x2 + 6 * Math.pow(x2, 2) - 2 * x1 - 3 * x2;
+
+const probl1Grad1 = (x1, x2) => 2 * x1 + 2 * x2 - 2;
+const probl1Grad2 = (x1, x2) => 2 * x1 + 12 * x2 - 3;
+
+const probl2 = (x1, x2) =>
+  Math.pow(E_CONSTANT, -(Math.pow(x1, 2) + Math.pow(x2, 2))) *
+  (Math.pow(x1, 2) + 6 * Math.pow(x2, 2));
+
+const probl2Grad1 = (x1, x2) => 2 * x1 + 2 * x2 - 2;
+const probl2Grad2 = (x1, x2) => 2 * x1 + 12 * x2 - 3;
+
+const functiaInitiala = probl1;
+const grad1 = (x1, x2) => 2 * x1 + 2 * x2 - 2;
+const grad2 = (x1, x2) => 2 * x1 + 12 * x2 - 3;
 
 const dubluModul = (a, b) =>
   Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)).toFixed(6);
@@ -18,31 +32,37 @@ const main = () => {
   // se calculeaza si initializeaza valoarea gradientul functiei
   let comparabila = dubluModul(valG1, valG2);
   let pasul = 1;
+  let iter = 1;
   // se intra intr-un ciclu cu conditia valoarea gradientului < Err, unde err e 10^-5
   while (!(comparabila <= err)) {
     // daca da, atunci se gasesc puncte noi
     let newX1, newX2;
     while (!newX1 && !newX2) {
+      iter++;
       const direction = comparabila > 0 ? -1 : 1;
       const newX1Tentative = x1 + pasul * valG1 * direction;
       const newX2Tentative = x2 + pasul * valG2 * direction;
 
       const valFuncitieNoi = functiaInitiala(newX1Tentative, newX2Tentative);
+      console.log(newX1Tentative, newX2Tentative, valFuncitieNoi);
       if (valFuncitieNoi < valoareaFunctieiInitiale) {
+        console.log('---------');
         const newValG1 = grad1(newX1Tentative, newX2Tentative);
         const newValG2 = grad2(newX1Tentative, newX2Tentative);
         comparabila = dubluModul(newValG1, newValG2);
+        console.log(newValG1, newValG2, comparabila);
+        console.log('=========');
         x1 = newX1Tentative;
         x2 = newX2Tentative;
         valG1 = newValG1;
         valG2 = newValG2;
         break;
       } else {
-        pasul *= 0.5;
+        pasul *= 0.1;
       }
     }
   }
-  console.log(`Acesta sunt punctul de minim al functiei: ${x1} si ${x2}`);
+  console.log(`Acesta sunt punctul de minim al functiei: ${x1} si ${x2}\n`);
 };
 
 main();
